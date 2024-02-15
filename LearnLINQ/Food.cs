a4ab35775c6ba1ac01754c9675f7b3dc9c7a6f34
voja <voja@gmail.com>
2024-02-15 10:49:35 +0100

    01 Start

---
+++ LearnLINQ/Food.cs
@@ -0,0 +1,15 @@
+﻿namespace LearnLINQ
+{
+    class Food(int id, string name, FoodType foodType, decimal price)
+    {
+        public int Id { get; set; } = id;
+        public string Name { get; set; } = name;
+        public FoodType FoodType { get; set; } = foodType;
+        public decimal Price { get; set; } = price;
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}, Type: {FoodType}, Price: {Price}";
+        }
+    }
+}
