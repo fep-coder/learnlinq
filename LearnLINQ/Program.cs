73b69c638e1c4d0473c37c5a7e2326a832483a93
voja <voja@gmail.com>
2024-02-15 11:21:21 +0100

    02 No linq vs linq

--- LearnLINQ/Program.cs
+++ LearnLINQ/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool IsAnyWordUppercase(IEnumerable<string> words)
+{
+    foreach (var word in words)
+    {
+        bool allAreUppercase = true;
+
+        foreach (var letter in word)
+        {
+            if (char.IsLower(letter))
+            {
+                allAreUppercase = false;
+            }
+        }
+
+        if (allAreUppercase)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+var wordsNoUppercase = new[] { "red", "green", "blue" };
+var wordsWithUppercase = new[] { "red", "GREEN", "blue" };
+
+Console.WriteLine(IsAnyWordUppercase(wordsNoUppercase));
+Console.WriteLine(IsAnyWordUppercase(wordsWithUppercase));
+
+bool IsAnyWordUppercaseLinq(IEnumerable<string> words)
+{
+    return words.Any(word => word.All(letter => char.IsUpper(letter)));
+}
+
+Console.WriteLine(IsAnyWordUppercaseLinq(wordsNoUppercase));
+Console.WriteLine(IsAnyWordUppercaseLinq(wordsWithUppercase));
+
+Console.ReadLine();
