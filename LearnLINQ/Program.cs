346c579460162a73af8361589cab34220fbe0b71
voja <voja@gmail.com>
2024-02-15 12:01:30 +0100

    05 Create our own linq like method 3

--- LearnLINQ/Program.cs
+++ LearnLINQ/Program.cs
@@ -1,32 +1,6 @@
-﻿bool isAnyGreaterThan100(int[] numbers)
-{
-    foreach (var number in numbers)
-    {
-        if (isGreaterThan100(number))
-        {
-            return true;
-        }
-    }
-
-    return false;
-}
-
-bool isGreaterThan100(int number)
+﻿bool isGreaterThan100(int number)
 {
     return number > 100;
-}
-
-bool isAnyEven(int[] numbers)
-{
-    foreach (var number in numbers)
-    {
-        if (isEven(number))
-        {
-            return true;
-        }
-    }
-
-    return false;
 }
 
 bool isEven(int number)
@@ -34,9 +8,45 @@ bool isEven(int number)
     return number % 2 == 0;
 }
 
+bool isAny(int[] numbers, Func<int, bool> predicate)
+{
+    foreach (var number in numbers)
+    {
+        if (predicate(number))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
 var numbers = new[] { 1, 4, 6, 29, 200, 50 };
 
-Console.WriteLine("isAnyGreaterThan100 " + isAnyGreaterThan100(numbers));
-Console.WriteLine("isAnyEven " + isAnyEven(numbers));
+bool isAnyGreaterThan100 = isAny(numbers, isGreaterThan100);
+bool isAnyEven = isAny(numbers, isEven);
+
+Console.WriteLine($"Is any > 100? {isAnyGreaterThan100}");
+Console.WriteLine($"Is any even? {isAnyEven}");
+
+bool isAnyGeneric<T>(IEnumerable<T> numbers, Func<T, bool> predicate)
+{
+    foreach (var number in numbers)
+    {
+        if (predicate(number))
+        {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+var words = new[] { "red", "green", "blue" };
+
+bool isAnyLength5 = isAnyGeneric(words, word => word.Length == 5);
+Console.WriteLine("isAnyLength5 " + isAnyLength5);
+
+bool isAnyZero = numbers.Any(x => x == 0);
 
 Console.ReadLine();
