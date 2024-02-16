6a17b72780860a60501854c91056b6bec7e4a183
voja <voja@gmail.com>
2024-02-16 10:11:01 +0100

    12 OrderBy

--- LearnLINQ/Data.cs
+++ LearnLINQ/Data.cs
@@ -2,7 +2,7 @@
 {
     class Data
     {
-        public static int[] Numbers = [1, 3, 6, 8, 12];
+        public static int[] Numbers = [1, 3, 6, 8, 100, 11, 12];
 
         public static string[] Words = ["lion", "tiger", "giraffe"];
 
--- LearnLINQ/Program.cs
+++ LearnLINQ/Program.cs
@@ -1,11 +1,24 @@
-﻿// Contains
+﻿// OrderBy
 
 using LearnLINQ;
 
-var isSixPresent = Data.Numbers.Contains(6);
-Console.WriteLine("isSixPresent " + isSixPresent);
+var foodOrderedByName = Data.Food.OrderBy(x => x.Name);
+Console.WriteLine("foodOrderedByName \n" + 
+                    string.Join("\n", foodOrderedByName));
 
-var isLionPresent = Data.Words.Contains("lion");
-Console.WriteLine("isLionPresent " + isLionPresent);
+var foodOrderedByNameDesc = Data.Food.OrderByDescending(x => x.Name);
+Console.WriteLine("foodOrderedByNameDesc \n" + 
+                    string.Join("\n", foodOrderedByNameDesc));
+
+var orderedNumbers = Data.Numbers.OrderBy(number => number);
+Console.WriteLine("orderedNumbers \n" +
+                    string.Join("\n", orderedNumbers));
+
+var foodOrderedByTypeThenName = 
+                                Data.Food
+                                .OrderBy(x => x.FoodType)
+                                .ThenBy(x => x.Name);
+Console.WriteLine("foodOrderedByTypeThenName \n" +
+                    string.Join("\n", foodOrderedByTypeThenName));
 
 Console.ReadLine();
