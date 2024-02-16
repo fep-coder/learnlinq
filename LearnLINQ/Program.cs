ef81d51d3321a621988c14e8ea78e28d9883bd2b
voja <voja@gmail.com>
2024-02-16 12:48:22 +0100

    28 ToLookup and GroupBy

--- LearnLINQ/Program.cs
+++ LearnLINQ/Program.cs
@@ -1,22 +1,37 @@
-﻿// Generating new collections
+﻿// ToLookup and GroupBy
 
 using LearnLINQ;
 
-var emptyInts = Enumerable.Empty<int>();
-if (!emptyInts.Any()) Console.WriteLine("Empty");
+var foodPriceByTypeLookup =
+    Data.Food
+        .ToLookup(
+            food => food.FoodType, // key
+            food => food.Price // value
+        );
 
-var tenCopies = Enumerable.Repeat(1, 10);
-Console.WriteLine("tenCopies " + string.Join(", ", tenCopies));
+var sumOfPricesByType =
+                    foodPriceByTypeLookup
+                        .ToDictionary(
+                            lookup => lookup.Key,
+                            lookup => lookup.Sum()
+                        );
 
-var copiesWithIndex = Enumerable
-                        .Repeat("banana", 3)
-                        .Select((x, i) => $"{i + 1}. {x}");
-Console.WriteLine("copiesWithIndex " + string.Join(", ", copiesWithIndex));
+Console.WriteLine("sumOfPricesByType " + string.Join(", ", sumOfPricesByType));
 
-var oneToTen = Enumerable.Range(1, 10);
-Console.WriteLine("oneToTen " + string.Join(", ", oneToTen));
+var groupings =
+    Data.Food
+        .GroupBy(
+            food => food.FoodType, // key
+            food => food.Price // value
+        );
 
-var defaultIfEmpty = Data.Numbers.DefaultIfEmpty();
-Console.WriteLine("defaultIfEmpty " + string.Join(", ", defaultIfEmpty));
+var sumOfPricesByType2 =
+                    foodPriceByTypeLookup
+                        .ToDictionary(
+                            x => x.Key,
+                            x => x.Sum()
+                        );
+
+Console.WriteLine("sumOfPricesByType2 " + string.Join(", ", sumOfPricesByType2));
 
 Console.ReadLine();
