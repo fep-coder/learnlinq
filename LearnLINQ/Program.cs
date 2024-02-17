e981079789563c8d14e200f793b60633adc16c47
voja <voja@gmail.com>
2024-02-17 09:01:45 +0100

    34 select query syntax

--- LearnLINQ/Program.cs
+++ LearnLINQ/Program.cs
@@ -1,21 +1,30 @@
-﻿// where query syntax 
+﻿// select query syntax 
 
 using LearnLINQ;
 
-var evenNumbers = from number in Data.Numbers
-                  where number % 2 == 0
-                  orderby number descending
-                  select number;
+var doubled = from number in Data.Numbers
+              select number * 2;
+Console.WriteLine("doubled \n" + string.Join("\n", doubled));
 
-Console.WriteLine("evenNumbers \n" + string.Join("\n", evenNumbers));
+var wordsUppercase = from word in Data.Words
+                     select word.ToUpper();
+Console.WriteLine("wordsUppercase \n" + string.Join("\n", wordsUppercase));
 
-var specificFood = (from food in Data.Food
-                    where
-                    (food.FoodType == FoodType.Meat ||
-                    food.FoodType == FoodType.Fruit) &&
-                    food.Price > 1 && food.Name.Length > 4
-                    select food).Count();
+var foodNames = from food in Data.Food
+                select food.Name;
+Console.WriteLine("foodNames \n" + string.Join("\n", foodNames));
 
-Console.WriteLine("specificFood \n" + string.Join("\n", specificFood));
+var pricesOverOne = (from food in Data.Food
+                     where food.Price > 1
+                     select food.FoodType).Distinct();
+Console.WriteLine("pricesOverOne \n" + string.Join("\n", pricesOverOne));
+
+var foodInfo = from food in Data.Food
+               orderby food.Name
+               select $"{food.Name.First()}. " +
+                       $"{food.FoodType} " +
+                       $"${food.Price}";
+
+Console.WriteLine("foodInfo \n" + string.Join("\n", foodInfo));
 
 Console.ReadLine();
