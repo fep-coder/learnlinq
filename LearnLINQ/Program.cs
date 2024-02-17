d5971651c02ad25904243167e358834d53fa84ad
voja <voja@gmail.com>
2024-02-17 08:53:02 +0100

    33 select query syntax

--- LearnLINQ/Program.cs
+++ LearnLINQ/Program.cs
@@ -1,24 +1,21 @@
-﻿// Query syntax and orderby
+﻿// where query syntax 
 
 using LearnLINQ;
 
-var orderedNumbers = from number in Data.Numbers
-                     let timesTwo = number * 2
-                     orderby timesTwo
-                     select timesTwo;
+var evenNumbers = from number in Data.Numbers
+                  where number % 2 == 0
+                  orderby number descending
+                  select number;
 
-Console.WriteLine("orderedNumbers \n" + string.Join("\n", orderedNumbers));
+Console.WriteLine("evenNumbers \n" + string.Join("\n", evenNumbers));
 
-var foodDescending = from food in Data.Food
-                     orderby food.Name descending
-                     select food;
+var specificFood = (from food in Data.Food
+                    where
+                    (food.FoodType == FoodType.Meat ||
+                    food.FoodType == FoodType.Fruit) &&
+                    food.Price > 1 && food.Name.Length > 4
+                    select food).Count();
 
-Console.WriteLine("foodDescending \n" + string.Join("\n", foodDescending));
-
-var foodByTypeAndId = (from food in Data.Food
-                       orderby food.FoodType, food.Id
-                       select food).Reverse();
-
-Console.WriteLine("foodByTypeAndId \n" + string.Join("\n", foodByTypeAndId));
+Console.WriteLine("specificFood \n" + string.Join("\n", specificFood));
 
 Console.ReadLine();
